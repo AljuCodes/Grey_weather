a2eae38d0056b035af968f67ab8587d8cbb556c6
Alju <aljithmadambichonedan@gmail.com>
2023-10-19 09:53:53 +0530

    Initial Commit

---
+++ Grey Weather/Grey_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grey_WeatherApp.swift
+//  Grey Weather
+//
+//  Created by FAO on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Grey_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
